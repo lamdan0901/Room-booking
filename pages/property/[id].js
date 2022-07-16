e400c5db88c51b172c29748e9bc93b0889169174
lamdan0901 <itmaster912@gmail.com>
2022-07-16 16:27:22 +0700

    try to fix build failure

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -6,7 +6,7 @@ import { GoVerified } from "react-icons/go";
 import millify from "millify";
 
 import { baseUrl, fetchApi } from "../../utils/fetchApi";
-import { ImageScrollbar } from "../../components/ImageScrollbar";
+import { ImageScrollBar } from "../../components/ImageScrollBar";
 
 const PropertyDetails = ({
   propertyDetails: {
@@ -27,7 +27,7 @@ const PropertyDetails = ({
   },
 }) => (
   <Box maxWidth="1000px" margin="auto" p="4">
-    {photos && <ImageScrollbar data={photos} />}
+    {photos && <ImageScrollBar data={photos} />}
 
     <Box w="full" p="6">
       <Flex paddingTop="2" alignItems="center">
